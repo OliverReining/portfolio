51fc790a6aef956b08721a0bc3f0834d31b60b46
Oliver Reining <oliver.reining@gmail.com>
2026-05-11 16:11:52 +0200

    feat: add footer component

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+<template>
+  <footer>
+    <h2>INITIATE<br />SEQUENCE</h2>
+    <a href="mailto:oliver.reining@gmail.com" class="contact-btn">OLIVER.REINING@GMAIL.COM</a>
+  </footer>
+</template>
+<style scoped></style>
