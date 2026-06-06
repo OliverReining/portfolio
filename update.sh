9cf4ea6b8e5bfa67afd1304a86f41c664e760674
Oliver Reining <oliver.reining@gmail.com>
2026-06-06 16:01:48 +0200

    feat: add update script to automate git pull and rebuild process

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Checking for updates..."
+GIT_OUTPUT=$(git pull)
+
+if [[ "$GIT_OUTPUT" == *"Already up to date."* ]]; then
+    echo "Everything is already up to date. No build needed!"
+    exit 0
+fi
+
+echo "New changes detected! Rebuilding..."
+docker compose up -d --build
+
+echo "Update successfully deployed!"cd
